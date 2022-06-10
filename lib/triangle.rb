db45230f24b22137d2fd166ebe11a6a85c689bf6
catherine <vuthi.esther@gmail.com>
2022-06-10 19:48:06 +0300

    commit tree f07b20de861ed3d4cf4eb8acc4eecba857bfae58

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    valid_triangle
+    if a == b && b == c
+      :equilateral
+      elsif a == b || a == c || b == c 
+        :isosceles
+      else
+        :scalene
+      end
+  end
+
+   def greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def valid_triangle
+    raise TriangleError unless greater_than_zero? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
